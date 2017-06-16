a5176f11b4c379fd019e1b8a14936ec73acbc9a7
Sven Bauersfeld <sven.bauersfeld@web.de>
2017-06-16 13:30:31 +0200

    initial steps like build.sbt

---
+++ src/main/scala/de/htwg/se/yooloo/aview/tui/gui/SwingGui.scala
@@ -0,0 +1,15 @@
+package de.htwg.se.yooloo.aview.tui.gui
+
+
+import de.htwg.se.yooloo.controller.Impl.IController
+
+import scala.swing.Frame
+
+/**
+  * Created by svenb on 16.06.2017.
+  */
+class SwingGui(controller: IController) extends Frame {
+
+
+
+}
