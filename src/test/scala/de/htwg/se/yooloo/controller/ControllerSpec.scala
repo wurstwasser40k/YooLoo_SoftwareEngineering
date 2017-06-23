a4f07f3785950194397524d165fb178645e08b5f
Viktoria1 <Viktoria.m.schreiner@gmail.com>
2017-06-23 13:29:34 +0200

    little additions

--- src/test/scala/de/htwg/se/yooloo/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/yooloo/controller/ControllerSpec.scala
@@ -12,95 +12,35 @@ class ControllerSpec extends WordSpec with Matchers {
     "adding a player" should {
       val player = new PlayerFactory().create("Viktoria")
       controller.addPlayer("Viktoria")
+
       "add player to List of player" in {
         controller.players.toString() should be("List(Name: Viktoria)")
       }
     }
-    "setting current player " should{
+    "setting current player " should {
+      "initially be no player " in {
+        controller.getNameCurrentPlayer() should be(null)
+      }
 
+      "be the first player in list" when {
+        "size of current players is > 2" in {
+          controller.addPlayer("Sven")
+          controller.players.size should be (2)
+          controller.setCurrentPlayer()
+          controller.getNameCurrentPlayer() should be ("Sven")
+        }
+      }
+
+      /*
+      "notify its Observer after card current player was set" in {
+      }
+      "notify its Observer after card was added" in {
+      }
+      "notify its Observer after move was evaluated" in {
+      }
+      "notify its Observer after round was evaluated" in {
+      }
+      */
     }
-    /*
-    "notify its Observer after card current player was set" in {
-    }
-    "notify its Observer after card was added" in {
-    }
-    "notify its Observer after move was evaluated" in {
-    }
-    "notify its Observer after round was evaluated" in {
-    }
-    */
   }
 }
-
-
-/*
-
-
-  "Calling makeAMove for " +
-    "set1.cardSet=List(5,4,3,2,1,10,9,8,7,6)" +
-    "set2.cardSet=List(5,4,3,2,1,10,9,8,7,6)" +
-    "set3.cardSet=List(5,10,1,2,3,4,6,8,7,9)" should {
-      "result in  55 points for the third player C while player1 and 2 have 0 points" in {
-
-        set1.cardSet = List(5, 4, 3, 2, 1, 10, 9, 8, 7, 6)
-        set2.cardSet = List(5, 4, 3, 2, 1, 10, 9, 8, 7, 6)
-        set3.cardSet = List(5, 10, 1, 2, 3, 4, 6, 8, 7, 9)
-        playingField.makeAMove()
-
-        player1.totalPoints should be(0)
-        player2.totalPoints should be(0)
-        player3.totalPoints should be(55)
-
-      }
-    }
-
-  "Calling decideWhoGetsThePoint(5,3,2,5) " should {
-    "result in  5 points  points for the first player A" in {
-
-      player1.totalPoints = 0
-      player2.totalPoints = 0
-      player3.totalPoints = 0
-
-      playingField.decideWhoGetsThePoint(3, 2, 5)
-
-      player1.totalPoints should be(5)
-      player2.totalPoints should be(0)
-      player3.totalPoints should be(0)
-
-
-    }
-
-  }
-
-  "Calling decideWhoGetsThePoint(4,4,4,5)   " should {
-    "result in 0 points for all players " in {
-
-      player1.totalPoints = 0
-      player2.totalPoints = 0
-      player3.totalPoints = 0
-
-      playingField.decideWhoGetsThePoint( 4, 4, 5)
-
-      player1.totalPoints should be(0)
-      player2.totalPoints should be(0)
-      player3.totalPoints should be(0)
-
-    }
-  }
-
-  "Calling decideWhoGetsThePoint(4,4,3,10)   " should {
-    "result in 10 points for player3 " in {
-
-      player1.totalPoints = 0
-      player2.totalPoints = 0
-      player3.totalPoints = 0
-
-      playingField.decideWhoGetsThePoint(4, 3, 10)
-
-      player1.totalPoints should be(0)
-      player2.totalPoints should be(0)
-      player3.totalPoints should be(10)
-
-    }
-  }
-  */
